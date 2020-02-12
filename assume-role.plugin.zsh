f1c4c335dd72cb80b7a55b9d8072bfaca4ebd596
Alan Jenkins <alan.jenkins@uk.clara.net>
2020-02-12 07:40:40 +0000

    Clean up vscode alias and fix whitespace.

--- assume-role.plugin.zsh
+++ assume-role.plugin.zsh
@@ -5,7 +5,8 @@ unset_aws() {
     unset AWS_SECRET_ACCESS_KEY;
     unset AWS_SESSION_TOKEN;
     unset AWS_SECURITY_TOKEN;
-  fi  
+    unset AWS_MFA_TOKEN;
+  fi
 }
 
 remove_role() {
@@ -14,9 +15,8 @@ remove_role() {
   unset AWS_SECRET_ACCESS_KEY;
   unset AWS_SESSION_TOKEN;
   unset AWS_SECURITY_TOKEN;
+  unset AWS_MFA_TOKEN;
 }
-
-alias vscode=code
 
 assume_role() {
   if [ -z "${_2fa}" ];then
@@ -24,7 +24,7 @@ assume_role() {
   else
     eval $(sudo 2fa ${_2fa} | assume-role $@ 2>&1 | grep -o "export .*")
   fi
-  export ASSUME_DURING=$(date -v +1H +%s); 
+  export ASSUME_DURING=$(date -v +1H +%s);
 }
 
 alias role_detail='function(){unset_aws;env |grep AWS_ACCESS_KEY_ID=;env |grep AWS_SECRET_ACCESS_KEY=;env |grep AWS_SESSION_TOKEN=;env |grep AWS_SECURITY_TOKEN=;}'
